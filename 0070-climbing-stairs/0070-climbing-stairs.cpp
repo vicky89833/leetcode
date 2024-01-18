d28d79dc6f5583ba8b652ae7a2825653cd67b2c3
vicky89833 <113588950+vicky89833@users.noreply.github.com>
2024-01-19 00:52:11 +0530

    Time: 3 ms (16.28%), Space: 6.4 MB (71.72%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    
+    int climbStairs(int n) {
+        vector<int>result(n+1,0);
+        result[0] = 1; 
+        result[1] = 1;
+        for(int i=2;i<=n;i++){
+           
+                result[i]=result[i-2]+result[i-1];
+            
+            
+        }
+        
+        return result[n];
+    }
+};
