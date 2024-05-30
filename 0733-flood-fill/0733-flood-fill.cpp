cbb8c5bb99410c495f414cf486c09e4e34cbde79
vicky kumar maurya <113588950+vicky89833@users.noreply.github.com>
2024-05-31 02:22:09 +0530

    Time: 4 ms (83.91%), Space: 17.1 MB (23.52%) - LeetHub

--- 0733-flood-fill/0733-flood-fill.cpp
+++ 0733-flood-fill/0733-flood-fill.cpp
@@ -1,26 +1,28 @@
 class Solution {
 public:
-    void f(vector<vector<int>>& image, int sr, int sc, int color , int rcolor ){
+    void f(vector<vector<int>>& image, int sr, int sc, int color , int rcolor ,vector<vector<bool>> &dp ){
         if( sr < 0 || sc < 0 || sr >= image.size() ||  sc >= image[0].size() ){
             return ;
         }
-        if( image[sr][sc] != rcolor ) return ;
         
+        
+        if( image[sr][sc] != rcolor ) return ;
+        if( dp[sr][sc] )return ;
         image[sr][sc] = color ;
         
-        f(image,  sr,      sc -1 ,  color ,  rcolor ) ;
-        f(image,  sr,      sc + 1 , color ,  rcolor ) ;
-        f(image,  sr -1 ,  sc  ,    color ,  rcolor ) ;
-        f(image,  sr + 1,  sc  ,    color ,  rcolor ) ;
+        f(image,  sr,      sc -1 ,  color ,  rcolor ,dp ) ;
+        f(image,  sr,      sc + 1 , color ,  rcolor , dp ) ;
+        f(image,  sr -1 ,  sc  ,    color ,  rcolor , dp ) ;
+        f(image,  sr + 1,  sc  ,    color ,  rcolor , dp ) ;
         
     }
     vector<vector<int>> floodFill(vector<vector<int>>& image, int sr, int sc, int color) {
         
         //for that operation we have to move four directions
         //(i , j - 1) ,(i , j + 1 ) ,( i-1 ,j) ,( i + 1 , j)
-        
-        if( color == image[sr][sc] ) return image ;
-        f(image,  sr,  sc  ,  color , image[sr][sc] ) ;
+        if(color == image[sr][sc]) return image;
+        vector<vector<bool>> dp( image.size()  , vector<bool>( image[0].size()  , false ) ) ;
+        f(image,  sr,  sc  ,  color , image[sr][sc] , dp) ;
         return image ;
         
             
